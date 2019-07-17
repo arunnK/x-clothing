a399861edc184cdfa3792887707309e6d7a7f795
Arun Kumar <arunkumar7617@gmail.com>
2019-07-17 23:23:07 +0400

    add sign in and up page, sign in component, custom button, form input components

---
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import "./sign-in-up.styles.scss";
+
+const SignInUpPage = () => <SignIn />;
+
+export default SignInUpPage;
